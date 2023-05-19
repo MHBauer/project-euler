0f744db739c7038b1fc970d8a5957174a0e1168e
Morgan Bauer <me@mhbauer.com>
2023-05-19 16:45:20 -0700

    brute force solution to 14 in python

--- python/MorganEuler.py
+++ python/MorganEuler.py
@@ -249,6 +249,43 @@ def euler13():
         acc += int(n) 
     return str(acc)[0:10]
 
+def euler14(max = 1_000_000):
+    """
+    The following iterative sequence is defined for the set of positive integers:
+
+    n → n/2 (n is even)
+    n → 3n + 1 (n is odd)
+
+    Using the rule above and starting with 13, we generate the following sequence:
+    13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+    It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+    Which starting number, under one million, produces the longest chain?
+
+    NOTE: Once the chain starts the terms are allowed to go above one million.
+    """
+    # brute force, go through each chain, and count the length of the path.
+    # second version, first optimization is to put the key, value being, number and path length.
+    maxLength = 0
+    maxLengthC = 2
+    for i in range(2, max):
+        length = 0
+        nextC = i
+        while nextC != 1:
+            length +=1
+            nextC = nextCollatz(nextC)
+        if length > maxLength:
+            maxLength = length
+            maxLengthC = i
+    return maxLengthC
+
+def nextCollatz(n: int):
+    if n % 2 == 0: # even
+        return n//2
+    else:
+        return 3*n + 1
+
 def findMinTriNum():
     i = 0
     while True:
